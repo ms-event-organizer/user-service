e49f7a71756b794860897631b87fcd9368aa2c59
Viktor Wagner <vvagner.viktor@yandex.ru>
2024-11-15 14:23:12 +0100

    Init commit

---
+++ src/main/java/meetup/user_service/MeetupUserService.java
@@ -0,0 +1,11 @@
+package meetup.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupUserService {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetupUserService.class, args);
+    }
+}
