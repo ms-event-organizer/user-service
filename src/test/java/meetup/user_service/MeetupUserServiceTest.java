d5b9deb2019462e4ef8885efda7b66b79c22055c
Galina Lobacheva <147919859+KoshanSky1@users.noreply.github.com>
2024-12-01 13:53:42 +0300

    fix: fix contextLoadsTest

--- src/test/java/meetup/user_service/MeetupUserServiceTest.java
+++ src/test/java/meetup/user_service/MeetupUserServiceTest.java
@@ -1,12 +1,19 @@
 package meetup.user_service;
 
+import meetup.user_service.user.controller.UserController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MeetupUserServiceTest {
 
+    @Autowired
+    private UserController userController;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        Assertions.assertThat(userController).isNotNull();
     }
 }
