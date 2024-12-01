fb7347c999eb9d451bad5b3b378e81a8a6c742a7
vvbakhanovich <vv.bakh@yandex.ru>
2024-12-01 21:57:50 +0300

    fix: Fix tests

--- src/test/java/meetup/user_service/MeetupUserServiceTest.java
+++ src/test/java/meetup/user_service/MeetupUserServiceTest.java
@@ -3,7 +3,6 @@ package meetup.user_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.utility.TestcontainersConfiguration;
 
 @SpringBootTest
 @Import(TestcontainersConfiguration.class)
